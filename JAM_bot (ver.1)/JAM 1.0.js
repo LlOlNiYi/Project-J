d4816aa938a92547ce17288d18cdaee0e4e821b6
LlOlNiYi <77988019+LlOlNiYi@users.noreply.github.com>
2021-01-27 02:41:22 +0900

    Update JAM 1.0.js

--- JAM_bot (ver.1)/JAM 1.0.js
+++ JAM_bot (ver.1)/JAM 1.0.js
@@ -5,10 +5,5 @@ const InoutPut = {}; //저장 및 읽기 관련 함수들이 들어갈 객체.
 const PreMsg = {}; //도배 방지용 객체.
 const Feel = {happy:70,sad:30,angry:10}; //감정 객체.
 const Info = {}; //무언가의 정보를 설명하는 객체.
-/*Feel에 포함된 함수들과 Feel의 정보 정의*/
-Info.Feel = Object.keys(Feel).sort((a, b)=>Feel[b]-Feel[a]);
-if (Info.Feel[0] == happy) Info.Feel = happy;
-if (Info.Feel[0] == sad) Info.Feel = sad;
-if (Info.Feel[0] == angry) Info.feel = angry;
 /*AI에 포함된 함수들 정의*/
 AI.say = function(msg, replier)
