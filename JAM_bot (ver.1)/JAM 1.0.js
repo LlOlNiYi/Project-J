a0ee1ff68f6ed18c2beb10f2cb775634f4551b3f
LlOlNiYi <77988019+LlOlNiYi@users.noreply.github.com>
2021-01-28 02:38:17 +0900

    Update JAM 1.0.js

--- JAM_bot (ver.1)/JAM 1.0.js
+++ JAM_bot (ver.1)/JAM 1.0.js
@@ -9,7 +9,7 @@ const Info = {}; //무언가의 정보를 설명하는 객체.
 /*AI에 포함된 함수들 정의*/
 AI.say = function(msg, replier) {//채팅 전송 관련 함수
   replier.reply (Info.Face + " {" + msg + ")");//표정이랑 말이랑 합쳐서 전송
-}
+};
 AI.getRandomChatData = function(room) { //랜덤하게 저장된 채팅을 하나 가져오는 함수
   var data = DB.readData(room); //해당 채팅방에서 수신된 메세지들을 읽어옴
   if (data == null) return null; //해당 채팅방에서 수신된 메세지가 없으면 null값 반환
@@ -22,7 +22,11 @@ AI.isValidData = function(data) { //배울 말과 배우지 않을 말을 구분
   for (var no = 0; no < noSave.length; n++) { //배우지 않도록 예외처리
     if (noSave[no].indexOf(data) != -1) return false; //배열 요소중 하나라도 포함되는게 있다면 false 반환
   }
-  return true; //포함되지 않는 경우 true 반환
+      noSave = ["\n", "/"]; //이 배열에 들어있는 내용이 포함된 채팅은 배우지 않음. (필터링)
+    for (var no = 0; n < noSave.length; n++) { //배열의 길이만큼 반복
+        if (noSave[no].indexOf(data) != -1) return false; //배열의 요소들 중 포함되는게 하나라도 있다면, false 반환
+    }
+    return true; //아니면 true 반환
 };
 AI.study = function(room, msg) { //수신된 채팅을 학습하는 함수
   var data = DB.readData(room); //먼저 저장되어있던 내용을 불러옴
@@ -30,16 +34,17 @@ AI.study = function(room, msg) { //수신된 채팅을 학습하는 함수
     DB.saveData(room, msg); //새로 저장
   } else { //저장된 내용이 있다면
     DB.saveData(room, data + "\n" + msg); //기존에 있던 내용 뒤에 엔터와 함께 새로 저장함
+    }
   };
   
   /*DB 객체에 포함된 함수들 정의*/
   DB.createDir = function() { //채팅이 저장될 폴더를 생성하는 함수
-    var folder = new java.io.File(sdcard + "/AI/"); //파일 인스턴스 생성
+    var folder = new java.io.File(SdCard + "/AI/"); //파일 인스턴스 생성
     folder.mkdirs(); //폴더 생성. 상위 폴더가 없으면, 상위 폴더도 생성
   };
   DB.saveData = function(name, msg) { //파일에 내용을 저장하는 함수
     try { //저장 실행
-      var file = new java.io.File(sdcard + "/AI/" + name + ".txt");
+      var file = new java.io.File(SdCard + "/AI/" + name + ".txt");
       var fos = new java.io.FileOutputStream(file);
       var str = new java.lang.String(msg);
       fos.Write(str.getBytes());
@@ -50,7 +55,7 @@ AI.study = function(room, msg) { //수신된 채팅을 학습하는 함수
   };
   DB.readData = function(name) { //파일에 저장된 내용을 불러오는 함수
     try { //불러오기 실행
-      var file = new java.io.File(sdcard + "/AI/" + name + ".txt");
+      var file = new java.io.File(SdCard + "/AI/" + name + ".txt");
       if (!file.exist()) return null; //파일이 없으면 null값 반환
       var fis = new java.io.FileInputStream(file);
       var isr = new java.io.InputStreamReader(fis);
@@ -68,3 +73,9 @@ AI.study = function(room, msg) { //수신된 채팅을 학습하는 함수
       Log.debug(e + " At:" + e.lineNumber);
     }
   };
+
+  /*전역에서 실행할 것들*/
+  DB.createDir(); //폴더생성
+  function response (room, msg, sender, isGroupChat, replier, ImageDB) {
+
+}
